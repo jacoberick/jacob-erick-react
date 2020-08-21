d1b0177e0d56ad90318bd0dac4eb7f59e8049673
Jacob White <jacobewhitedev@gmail.com>
2020-08-21 15:59:52 -0500

    Updates

---
+++ precache-manifest.ed58adf1572e54f07a34154f3d2d944d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04dde615123876049b0cbe7d9214e9ae",
+    "url": "/jacob-erick-react/index.html"
+  },
+  {
+    "revision": "95e9f48efa74eab1fd11",
+    "url": "/jacob-erick-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "472afa1b4a8ff44e9a40",
+    "url": "/jacob-erick-react/static/css/main.ff736100.chunk.css"
+  },
+  {
+    "revision": "95e9f48efa74eab1fd11",
+    "url": "/jacob-erick-react/static/js/2.2f33bf77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jacob-erick-react/static/js/2.2f33bf77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472afa1b4a8ff44e9a40",
+    "url": "/jacob-erick-react/static/js/main.a43ee4f5.chunk.js"
+  },
+  {
+    "revision": "b89543fdb8db88296f4b",
+    "url": "/jacob-erick-react/static/js/runtime-main.109f61da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jacob-erick-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43152055ca969bd131368d912225b3a5",
+    "url": "/jacob-erick-react/static/media/skellygif.43152055.gif"
+  },
+  {
+    "revision": "efbfc100ac41f987df4375c6fa1e87d0",
+    "url": "/jacob-erick-react/static/media/webmain_red.efbfc100.jpg"
+  }
+]);
